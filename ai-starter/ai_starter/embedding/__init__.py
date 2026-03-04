ef55ef1141227901ad3a3b41351ba2afc8b23b53
bluemacaw-zhou <arcticfox.zhou@gmail.com>
2026-03-04 14:56:14 +0800

    Update __init__.py

--- ai-starter/ai_starter/embedding/__init__.py
+++ ai-starter/ai_starter/embedding/__init__.py
@@ -1,1 +1,14 @@
+"""
+Embedding 模块
+"""
+
+from ai_starter.embedding.embedding_interface import EmbeddingInterface
+from ai_starter.embedding.embedding_glm import GLMEmbedding
+from ai_starter.embedding.embedding_openai import OpenAIEmbedding
+
+__all__ = [
+    "EmbeddingInterface",
+    "GLMEmbedding",
+    "OpenAIEmbedding",
+]
 
